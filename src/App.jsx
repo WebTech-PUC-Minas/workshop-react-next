2fab7912735441c7d33a378b9f852ac91bb359ed
NilsonDeon <nilsondeon01@gmail.com>
2024-10-25 18:53:51 -0300

    Feat: remove arquivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,6 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import About from "./pages/About";
-import Home from "./pages/Home";
-import Navbar from './components/Navbar';
-
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" Component={Home} />
-        <Route path="/about" Component={About} />
-      </Routes>
-    </Router>
+    <h1 className="welcome"><span>Bem-Vindo</span> ao <span>Workshop</span></h1>
   );
 }
 
